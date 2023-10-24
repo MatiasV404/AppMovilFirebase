e36c4038296910dab58262ed3e32516a83bd99c2
ElMatyVeli <ma.velizi@duocuc.cl>
2023-10-24 15:00:04 -0300

    Creación de proyecto en Ionic Angular.

---
+++ app-alimentacion/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'app-alimentacion',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
