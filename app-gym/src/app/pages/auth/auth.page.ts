728be9269625828b3c43a88f303ef526c6375aef
ElMatyVeli <ma.velizi@duocuc.cl>
2023-10-26 21:05:08 -0300

    Validaciones en Login añadidas.

--- app-gym/src/app/pages/auth/auth.page.ts
+++ app-gym/src/app/pages/auth/auth.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { User } from 'src/app/models/user.model';
 import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-auth',
@@ -18,13 +19,37 @@ export class AuthPage implements OnInit {
   // Integramos el servicio de FireBase
   firebaseSvc = inject(FirebaseService);
 
+  // Integramos el loading
+  utilsSvc = inject(UtilsService);
+
   ngOnInit() {
   }
 
-  submit() {
-    if (this.form.valid){
+  // Función asíncrona
+  async submit() {
+    if (this.form.valid) {
+
+      const loading = await this.utilsSvc.loading();
+      await loading.present();
+
       this.firebaseSvc.singIn(this.form.value as User).then(res => {
-        console.log(res)
+        console.log(res);
+        // Capturamos errores
+      }).catch(error => {
+        console.log(error);
+
+        // Mostramos notificación al usuario
+        this.utilsSvc.presentToast({
+          message: 'Correo y/o contraseña inválido(s). Intente nuevamente.',
+          duration: 2500,
+          color: 'primary',
+          position: 'middle',
+          icon: 'alert-circle-outline'
+        })
+
+        // Quitamos el loading cuando termine la función
+      }).finally(() => {
+        loading.dismiss();
       })
     }
   }
