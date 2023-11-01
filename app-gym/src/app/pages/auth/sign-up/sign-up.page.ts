062764e505f5e2ee5de48671ace6361e9da410f0
ElMatyVeli <ma.velizi@duocuc.cl>
2023-11-01 20:02:39 -0300

    Registro completado

--- app-gym/src/app/pages/auth/sign-up/sign-up.page.ts
+++ app-gym/src/app/pages/auth/sign-up/sign-up.page.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/models/user.model';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.page.html',
   styleUrls: ['./sign-up.page.scss'],
 })
+
 export class SignUpPage implements OnInit {
 
-  constructor() { }
+    form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+    name: new FormControl('', [Validators.required, Validators.minLength(4)]),
+  })
+
+  // Integramos el servicio de FireBase
+  firebaseSvc = inject(FirebaseService);
+
+  // Integramos el loading
+  utilsSvc = inject(UtilsService);
 
   ngOnInit() {
+  }
+
+  // Función asíncrona
+  async submit() {
+    if (this.form.valid) {
+
+      const loading = await this.utilsSvc.loading();
+      await loading.present();
+
+      this.firebaseSvc.singUp(this.form.value as User).then(async res => {
+        // Actualizamos el usuario
+        await this.firebaseSvc.updateUser(this.form.value.name)
+        console.log(res);
+        // Capturamos errores
+      }).catch(error => {
+        console.log(error);
+
+        // Mostramos notificación al usuario
+        this.utilsSvc.presentToast({
+          message: 'Correo y/o contraseña inválido(s). Intente nuevamente.',
+          duration: 2500,
+          color: 'primary',
+          position: 'middle',
+          icon: 'alert-circle-outline'
+        })
+
+        // Quitamos el loading cuando termine la función
+      }).finally(() => {
+        loading.dismiss();
+      })
+    }
   }
 
 }
--- app-gym/src/app/services/firebase.service.ts
+++ app-gym/src/app/services/firebase.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { User } from '../models/user.model';
 
 @Injectable({
@@ -12,8 +12,18 @@ export class FirebaseService {
   // Autenticación
 
   // Acceder
-  singIn(user : User){
+  singIn(user: User) {
     return signInWithEmailAndPassword(getAuth(), user.email, user.password)
+  }
+
+  // Crear usuario
+  singUp(user: User) {
+    return createUserWithEmailAndPassword(getAuth(), user.email, user.password)
+  }
+
+  // Actualizar usuario
+  updateUser(displayName: string){
+    return updateProfile(getAuth().currentUser, { displayName })
   }
 
 }
