9007556da0b5502a241e88af27e75e510776aa5c
ElMatyVeli <ma.velizi@duocuc.cl>
2023-11-02 12:22:41 -0300

    Función cerrar sesión aplicada.

--- app-gym/src/app/services/firebase.service.ts
+++ app-gym/src/app/services/firebase.service.ts
@@ -4,6 +4,7 @@ import { createUserWithEmailAndPassword, sendPasswordResetEmail, getAuth, signIn
 import { User } from '../models/user.model';
 import { AngularFirestore } from '@angular/fire/compat/firestore'
 import { getFirestore, setDoc, doc, getDoc } from '@angular/fire/firestore'
+import { UtilsService } from './utils.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +12,11 @@ import { getFirestore, setDoc, doc, getDoc } from '@angular/fire/firestore'
 export class FirebaseService {
   auth = inject(AngularFireAuth);
   firestore = inject(AngularFirestore);
+  utilsSvc = inject(UtilsService);
 
   // Autenticación
 
-  getAuth(){
+  getAuth() {
     return getAuth();
   }
 
@@ -36,6 +38,14 @@ export class FirebaseService {
   // Restablecer contraseña
   sendRecoveryEmail(email: string) {
     return sendPasswordResetEmail(getAuth(), email)
+  }
+
+  // Cerrar sesión
+  signOut() {
+    getAuth().signOut();
+    // Removemos el usuario del almacenamiento local
+    localStorage.removeItem('user');
+    this.utilsSvc.routerLink('/auth');
   }
 
   // Base de datos
